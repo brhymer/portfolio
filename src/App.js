15d7bf52e526552e68280c8c905fc97b3a848b84
Brhymer <leiqien28@hotmail.com>
2020-06-28 15:26:29 -0700

    switched index.css and App.css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
 
-import './App.css';
+import './index.css';
 
 function App() {
   return (
