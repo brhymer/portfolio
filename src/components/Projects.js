fe97c0523e9e53fde8aa19981a22a82f73b12857
Brhymer <leiqien28@hotmail.com>
2020-07-11 09:47:06 -0700

    adds to proj descriptions

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,7 @@ class Projects extends Component {
     state ={
         projects: [
             {title: "HoneyDew", 
-            description: "An app for keeping track of tasks that need doing around the home.  Allows the creation of a hierarchy of spaces and subspaces each with their own tasks.", 
+            description: "An app for keeping track of tasks that need doing around the home.  Allows the creation of a hierarchy of spaces and subspaces each with their own tasks. This project makes use of several dependencies to allow posting and deletion of images to go with the tasks and the profile.", 
             tech: "A full-stack app using Mongo DB and Express",
             github: "https://git.generalassemb.ly/BrR/HoneyDew",
             live: "https://ga-honeydew-tasks.azurewebsites.net/spaces/",
@@ -21,7 +21,7 @@ class Projects extends Component {
             image2: honeydew2,
         },
             {title: "Tutor Marketplace", 
-            description: "A space for language students to connect with teachers.  Teachers can set their classes and rates, and students can sign up, and both have their own profile pages.", 
+            description: "A space for language students to connect with teachers.  Teachers can set their classes and rates, and students can sign up, and both have their own profile pages. This project could be expanded to include teachers of other subjects as well.", 
             tech: "A full-stack app using Python and Django",
             github: "https://git.generalassemb.ly/BrR/TutorMarketplace",
             live: "https://tutor-marketplace-sei.herokuapp.com/",
@@ -29,7 +29,7 @@ class Projects extends Component {
             image2: tutor2,
         },
             {title: "Tabula Aurea", 
-            description: "A tool for tracking your monthly income and expenses, assets and liabilities, as well as your financial goals.  Also provides the current prices for gold, silver and any stock the user enters.", 
+            description: "A tool for tracking your monthly income and expenses, assets and liabilities, as well as your financial goals.  Also provides the current prices for gold, silver and any stock the user enters. I enjoyed putting this together and learned a good deal about JS filtering and mapping.", 
             tech: "A full-stack MERN app using Chartjs and external APIs",
             github: "https://git.generalassemb.ly/BrR/Tabula-aurea-frontend",
             live: "https://tabulaaurea.herokuapp.com/",
@@ -37,7 +37,7 @@ class Projects extends Component {
             image2: tabula2
         },
             {title: "The Legend of Nelda", 
-            description: "A tongue-in-cheek homage to the NES classic, Nelda boasts a custom battle system, a variety of treasures and weapons, 6 levels, 1 secret area and 4 possible endings.", 
+            description: "A tongue-in-cheek homage to the NES classic, Nelda boasts a custom battle system, a variety of treasures and weapons, 6 levels, 1 secret area and 4 possible endings. This one was a lot of fun putting together, and has the potential for nearly endless expansion.", 
             tech: "An in-browser game using JQuery, Javascript, and HTML",
             github: "https://git.generalassemb.ly/BrR/The-Legend-of-Nelda",
             live: "https://brhymer.github.io/Project-zero/",
