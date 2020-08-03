61079a619cee7c7e1d9fc726771b1a3f39a18a88
Brhymer <leiqien28@hotmail.com>
2020-08-03 10:27:47 -0700

    fixes small typo

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
             <p className="about">I am a software developer with an emphasis on the frontend.  I work in JavaScript (node.js, jQuery, and react.js), Python (django), and PHP.  I am skilled with CSS and HTML, and I also have experience with MongoDB and PostgreSQL. I can build your frontend, backend, or fullstack application alone or as part of a team.</p>
             <h2 className="abouth" style={{textIndent: "26%"}}>My Interests</h2>
             <p className="about">I am a lifelong student of natural languages.  I am fluent in Japanese, have a modest comprehension of Spanish, French, Thai, and Mandarin.  and basic knowledge of Russian, German, and Korean.</p>
-            <p className="about">I am avid reader.  Some of my favorite non-fiction writers are Carl Sagan, Nassim Taleb, and Yuval Noah Harari. Some of my favorite fiction writers are Margaret Atwood, John Scalzi, Neil Gaiman, and Stephen King.</p>
+            <p className="about">I am an avid reader.  Some of my favorite non-fiction writers are Carl Sagan, Nassim Taleb, and Yuval Noah Harari. Some of my favorite fiction writers are Margaret Atwood, John Scalzi, Neil Gaiman, and Stephen King.</p>
             <p className="about">I love gaming, favorites including the Civilization series, EU4, and the Dark Souls series.</p>
             <p className="about">In my free time, I enjoy cooking, as well as archery, hiking, and rock climbing.</p>
             <h2 className="abouth" style={{textIndent: "44%"}}>Where I'm coming from</h2>
